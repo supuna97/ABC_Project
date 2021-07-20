84e0c0bdf092c2e2842e7afcae69f2406f125fb0
Supun Nanayakkara <supunnanayakkara01@gmail.com>
2021-07-20 13:16:09 +0530

    added 404 not found page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta name="description" content="">
+  <meta name="author" content="www.skylinewebsolutions.lk">
+  <title>404 Not found Error</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+  <link href="../css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-12">
+        <h1><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 404</h1>
+        <h2>Oops... Page Not Found!</h2>
+
+        <br>
+        <p>If you think you have arrived here by our mistake, please <a href="{{url('srcd/index')}}">Back to home</a></p>
+
+        <h3>Follow us:</h3>
+        
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          &copy; 2021 All Rights Reserved <a href="#" target="_blank">Powered by Supun Nanayakkara.</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
